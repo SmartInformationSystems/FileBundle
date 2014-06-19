ef598c09978ebdbda305218ec7a7a7a557c6a77c
YK115 <kurzhumov@gmail.com>
2014-06-19 21:26:40 +0600

    Update DefaultControllerTest.php

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SmartSystems\FileBundle\Tests\Controller;
+namespace SmartInformationSystems\FileBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
