7c851e45d82ccf43014397a3ab7adbbb458fb5bd
YK115 <kurzhumov@gmail.com>
2014-06-19 21:19:08 +0600

    Update and rename SmartSystemsFileExtension.php to SmartInformationSystemsFileExtension.php

--- DependencyInjection/SmartInformationSystemsFileExtension.php
+++ DependencyInjection/SmartInformationSystemsFileExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SmartSystems\FileBundle\DependencyInjection;
+namespace SmartInformationSystems\FileBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SmartSystemsFileExtension extends Extension
+class SmartInformationSystemsFileExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -25,7 +25,7 @@ class SmartSystemsFileExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-        $configurationContainer = $container->getDefinition('smart_systems_file.storage.configuration_container');
+        $configurationContainer = $container->getDefinition('smart_information_systems_file.storage.configuration_container');
         $configurationContainer->addMethodCall('setConfig', array($config));
     }
 }
